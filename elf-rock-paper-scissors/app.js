48cad38923a5bc722c6eb2c76b82a3afc5e4b9ea
Facundo Bremermann <fbremermann@ica.com.uy>
2022-12-02 16:18:43 -0300

    Dia 1 2022 ej 2 terminado

--- elf-rock-paper-scissors/app.js
+++ elf-rock-paper-scissors/app.js
@@ -1,8 +1,5 @@
 const fs = require('fs')
 
-//PART 1
-//=========================================================================
-
 let me = 1
 let opponent = 0
 
@@ -17,18 +14,72 @@ const stratString = String(fs.readFileSync(__dirname + '/input.txt'))
 
 const stratArr = stratString.split("\n").map(strat => strat.split(" "))
 
+//PART 1
+//=========================================================================
+
+// stratArr.forEach(strat => {
+
+//     switch (strat[me]) {
+//         case "X": shapeScore += 1
+//             break;
+//         case "Y": shapeScore += 2
+//             break;
+//         case "Z": shapeScore += 3
+//             break;
+//     }
+
+//     switch (strat[opponent]) {
+//         case "A": 
+//             if (strat[me] === "X") outcomeScore += 3;
+//             else if (strat[me] === "Y") outcomeScore += 6;
+//             else if (strat[me] === "Z") outcomeScore += 0;
+//             break;
+//         case "B":
+//             if (strat[me] === "X") outcomeScore += 0;
+//             else if (strat[me] === "Y") outcomeScore += 3;
+//             else if (strat[me] === "Z") outcomeScore += 6;
+//             break;
+//         case "C": 
+//             if (strat[me] === "X") outcomeScore += 6;
+//             else if (strat[me] === "Y") outcomeScore += 0;
+//             else if (strat[me] === "Z") outcomeScore += 3;
+//             break;
+//     }
+// })
+
+// console.log(shapeScore + outcomeScore)
+
+//PART 2
+//=========================================================================
+
 stratArr.forEach(strat => {
 
     switch (strat[me]) {
-        case "X": shapeScore += 1
+        case "X": outcomeScore += 0
             break;
-        case "Y": shapeScore += 2
+        case "Y": outcomeScore += 3
             break;
-        case "Z": shapeScore += 3
+        case "Z": outcomeScore += 6
             break;
     }
 
-    
+    switch (strat[opponent]) {
+        case "A": 
+            if (strat[me] === "X") shapeScore += 3;
+            else if (strat[me] === "Y") shapeScore += 1;
+            else if (strat[me] === "Z") shapeScore += 2;
+            break;
+        case "B":
+            if (strat[me] === "X") shapeScore += 1;
+            else if (strat[me] === "Y") shapeScore += 2;
+            else if (strat[me] === "Z") shapeScore += 3;
+            break;
+        case "C": 
+            if (strat[me] === "X") shapeScore += 2;
+            else if (strat[me] === "Y") shapeScore += 3;
+            else if (strat[me] === "Z") shapeScore += 1;
+            break;
+    }
 })
 
 console.log(shapeScore + outcomeScore)
