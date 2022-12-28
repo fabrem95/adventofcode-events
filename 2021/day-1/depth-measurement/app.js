afdcc77fdd2adf224ab9f81e1f8a8377ead4031a
Facundo Bremermann <fbremermann@ica.com.uy>
2022-12-27 21:35:09 -0300

    2021 day 1 complete

--- 2021/day-1/depth-measurement/app.js
+++ 2021/day-1/depth-measurement/app.js
@@ -24,12 +24,13 @@ const measurementsArr = measurementsString.split("\n").map(m => Number(m))
 const measurementsTrios = measurementsArr.map((m, i) => measurementsArr.slice(i, i+3)).map(trio => {
     let trioVal = 0
     for (const m of trio) {
-        
+        trioVal += m
     }
+    return trioVal;
 })
 
-for (let i = 0; i < measurementsArr.length; i++) {
-    if (i > 0 && measurementsArr[i] > measurementsArr[i-1]) largerMeasurements++
+for (let i = 0; i < measurementsTrios.length; i++) {
+    if (i > 0 && measurementsTrios[i] > measurementsTrios[i-1]) largerMeasurements++
 }
 
-console.log(measurementsTrios)
+console.log(largerMeasurements)
